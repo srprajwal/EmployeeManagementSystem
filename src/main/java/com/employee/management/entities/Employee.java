a664722dd9fa87a46deff5f6c47c16cdcc83a595
srpraju <srpraju1554@gmail.com>
2025-03-17 15:25:57 +0530

    Employee Class

---
+++ src/main/java/com/employee/management/entities/Employee.java
@@ -0,0 +1,28 @@
+package com.employee.management.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Table (name="employees")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Employee {
+@Id
+@GeneratedValue (strategy=GenerationType.IDENTITY)
+@Column (name="emp_id")
+private int empId;
+@Column (name="emp_name")
+private String empName;
+@Column (name = "designation")
+private String designation;
+@Column (name="emp_salary")
+private double empSalary;
+}
